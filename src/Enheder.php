e7e1ef3df93ee28fdbda67ef8eeaae834f7e9384
Arne Jørgensen <arne@arnested.dk>
2017-11-07 21:51:54 +0100

    Support using MS internal ID for "Enhed"
    A organization code is not always assigned or not assigned
    immediately. We now support using the Odoo internal ID to select from.

--- src/Enheder.php
+++ src/Enheder.php
@@ -22,39 +22,53 @@ class Enheder
     }
 
     /**
-     * Get an Enhed by it's ID (i.e. "2227-5").
+     * Get an Enhed by it's ID (i.e. "8960").
      *
-     * @param string $enhedId The Enhed ID to lookup by (i.e. "2227-5").
+     * @param string $enhedId The Enhed ID to lookup by (i.e. "8960").
      *
      * @return Enhed
      */
     public function getById(string $enhedId)
     {
-        // Preferably use an cached version.
-        if (empty($this->collection[$enhedId])) {
-            $this->requestEnhed($enhedId);
-        }
+        return $this->requestEnhed($enhedId, 'id');
+    }
 
-        return $this->collection[$enhedId];
+    /**
+     * Get an Enhed by it's organization code (i.e. "2227-5").
+     *
+     * @param string $organizationCode The Enhed organization code to lookup by (i.e. "2227-5").
+     *
+     * @return Enhed
+     */
+    public function getByOrganizationCode(string $organizationCode)
+    {
+        return $this->requestEnhed($organizationCode, 'organization_code');
     }
 
     /**
      * Lookup an Enhed by it's ID (i.e. "2227-5") from Odoo.
      *
-     * @param string $enhedId The Enhed ID to lookup by (i.e. "2227-5").
+     * @param string $enhedId The Enhed value to lookup by (i.e. "2227-5").
+     * @param string $field The Enhed field to lookup in (i.e. "id" or "organisation_code").
+     *
+     * @return Enhed
      */
-    protected function requestEnhed(string $enhedId)
+    protected function requestEnhed(string $value, string $field)
     {
-        $criteria = [['organization_code', '=', $enhedId]];
+        if (!empty($this->collection[$field][$value])) {
+            return $this->collection[$field][$value];
+        }
+
+        $criteria = [[$field, '=', $value]];
 
         $organizationCode = $this->odooClient->search('member.organization', $criteria);
 
         if (empty($organizationCode)) {
-            $this->collection[$enhedId] = null;
-
-            return;
+            return null;
         }
 
-        $this->collection[$enhedId] = new Enhed($this->odooClient, reset($organizationCode), $enhedId);
+        $this->collection[$field][$value] = new Enhed($this->odooClient, reset($organizationCode), $value);
+
+        return $this->collection[$field][$value];
     }
 }
